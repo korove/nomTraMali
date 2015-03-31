e90c03f90a84e827153af61595a5a4afd5ba50fd
korove <undfined1@gmail.com>
2015-03-31 11:43:42 +0700

    change path to assets

--- nut/app/views/layouts/master.blade.php
+++ nut/app/views/layouts/master.blade.php
@@ -3,22 +3,16 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Admin page</title>
-    <link rel="stylesheet" href="./bootstrap-3.3.4/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/bootstrap-3.3.4/dist/css/bootstrap.min.css">
 
     <!-- Optional theme -->
-    <link rel="stylesheet" href="./bootstrap-3.3.4/dist/css/bootstrap-theme.min.css">
-    <script src="./bootstrap-3.3.4/dist/js/jquery1.11.js"></script>
-    <script src="./bootstrap-3.3.4/dist/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="./css/home.css">
+    <link rel="stylesheet" href="./assets/css/bootstrap-3.3.4/dist/css/bootstrap-theme.min.css">
+    <script src="./assets/css/bootstrap-3.3.4/dist/js/jquery1.11.js"></script>
+    <script src="./assets/css/bootstrap-3.3.4/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="./assets/css/home.css">
   </head>
 
   <body>
-    <nav class="navigation">
-      @section('navigation')
-        <a href="/">Home</a>
-        <a href="/about">About2</a>
-      @show
-    </nav>
 
     <div id="header">
        <div ><img src="./assets/images/logomali.jpg" style="width:120px;" /></div>
